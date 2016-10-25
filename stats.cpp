6cb3ff3a57d9661d01f41b3ce8737163664ac708
ravenusmc <mcuddy77@gmail.com>
2016-10-25 09:30:15 -0400

    update, project almost completed

--- stats.cpp
+++ stats.cpp
@@ -18,16 +18,6 @@
 using namespace std;
 
 //*******************************
-//Structures
-//*******************************
-
-
-//*******************************
-//Global Variables/constants
-//*******************************
-
-
-//*******************************
 //Prototype Functions
 //*******************************
 
@@ -56,13 +46,17 @@ int main(){
     // This line will get the number of students surveyed for the size of the array.
     cout << "Please enter the number of students surveyed: " << endl;
     cin  >> students;
+    while (!(students > 0)){
+        cout << "The number of students must be positive!" << endl;
+        cin  >> students;
+    }
     
     //Creating the array based on the size of the students
     survey = new int[students];
     
     //These functions will basically drive the program.
     numMovies(survey, students);
-    //sortArray(survey, students);
+    sortArray(survey, students);
     average = getAverage(survey, students);
     mode = getMode(survey, students);
     median = getMedian(survey, students);
@@ -93,26 +87,50 @@ void numMovies(int *array, int size) {
     cout << "Please input the number of movies each student watched: " << endl;
     for (int i = 0; i < size; i++){
         cout << "student " << (i + 1) << ": ";
-        cin >> *(array + i);
+        cin >> array[i];
+        //cin >> *(array + i);
+        while ( array[i]  < 0 ){
+            cout << "value cannot be below one!" << endl;
+            cout << "student " << (i + 1) << ": ";
+            cin >> array[i];
+        }
     }
 
 }//End of numMovies function
 
 //This function will sort the array will will be helpful down the line for the statistical aspects
 void sortArray(int *array, int size){
+    
+    bool swap;
+    int temp;
+    
+    do
+    {
+        swap = false;
+        for (int count = 0; count < (size -1 ); count++ ){
+            if (array[count] > array[count + 1]){
+                temp = array[count];
+                array[count] = array[count + 1];
+                array[count + 1] = temp;
+                swap = true;
+            }
+        }
+    }while (swap);
 
-
-}//Enf of sortArray function.
+}//End of sortArray function.
 
 //This function will get the average of the array
 double getAverage(int *array, int size){
     
     double avg;
-    int total = 0;
+    double total = 0;
     
     for (int i = 0; i < size; i++){
         total += *(array + i);
     }
+    
+    cout << "total: " << total << endl;
+    cout << "size: " << size << endl;
     
     avg = total /size;
 
@@ -155,55 +173,54 @@ double getMode(int *array, int size){
     
 } // End of mode function
 
-
+//This function will get the median from the data.
 double getMedian(int *array, int size){
-
-    //Variables to find the median
-    int odd;
-    double odd_median;
-    double num_one;
-    double num_two;
-    double even_median;
-    double median;
-
-    if (size % 2 != 0){
-        odd = (size + 1) / 2;
-        odd_median = array[odd - 1];
-        return odd_median;
-    }else if (size % 2 == 0){
-        num_one = (size - 1) / 2;
-        num_two = ((size - 1) / 2) + 1;
-        even_median = (num_one + num_two) / 2;
-        return even_median;
+    
+    int middle = (size -1 ) / 2;
+    double Median;
+    
+    if (size % 2 == 0){
+        Median = (*(array + middle) + *(array + (middle + 1))) / 2;
+    }else {
+        Median = *(array + middle);
     }
     
-    return median;
-}
+    return Median;
 
+    //Variables to find the median
+//    int odd;
+//    double odd_median;
+//    double num_one;
+//    double num_two;
+//    double even_median;
+//    double median;
+    
+//    cout << "size: " << size << endl;
+//
+//    if (size % 2 != 0){
+//        odd = (size + 1) / 2;
+//        odd_median = array[odd - 1];
+//        return odd_median;
+//    }else if (size % 2 == 0){
+//        num_one = (size / 2) - 1;
+//        cout << array[num_one];
+//        //num_two = ((size - 1) / 2) + 1;
+//        num_two = (size / 2) + 1;
+//        //even_median = (array[num_one] + array[num_two]) / 2;
+//        return even_median;
+//    }
+    
+}//End of median function
+
+//This function will display the average, mode and median.
 void displayData(double avg, double mode, double med){
     
     cout << "Here is the results of the data: " << endl;
-    cout << "The average was: " << avg;
-    cout << "The mode was: " << mode;
-    cout << "The median was: " << med;
+    cout << "The average was: " << avg << endl;
+    cout << "The mode was: " << mode << endl;
+    cout << "The median was: " << med << endl;
     
-}
-
-
-//Write a program that can be used to gather statistical data about the number of movies college students see in a month. The program should perform the following steps:
-//
-//A) Ask the user how many students were surveyed. An array of integers with this
-//many elements should then be dynamically allocated.
-
-//B) Allow the user to enter the number of movies each student saw into the array.
-
-//C) Calculate and display the average, median, and mode of the values entered (see below).
-
-
-//In statistics, when a set of values is sorted in ascending or descending order, its median is the middle value. If the set contains an even number of values, the median is the mean, or average, of the two middle values.
-
-//The mode of a set of values is the value that occurs most often or with the
-//greatest frequency.
+}//End of displayData function
 
 
 //Input Validation: Do not accept negative numbers for input.
